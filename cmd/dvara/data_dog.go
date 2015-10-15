718f5a59893b106a4f6080d1f5e93c8179829498
Barisa Obradovic <barisa@intercom.io>
2015-10-15 16:15:28 +0100

    Moved datadog metrics to separate file

---
+++ cmd/dvara/data_dog.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/DataDog/datadog-go/statsd"
+)
+
+func NewDataDogStatsDClient(address string) DatadogStatsClient {
+	c, err := statsd.New(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return DatadogStatsClient{c}
+}
+
+type DatadogStatsClient struct {
+	client *statsd.Client
+}
+
+func (c DatadogStatsClient) BumpAvg(key string, val float64) {
+	// average can go up or down, so I gues gauge is best aproximate
+	c.client.Gauge(key, val, nil, 1)
+}
+
+func (c DatadogStatsClient) BumpHistogram(key string, val float64) {
+	c.client.Histogram(key, val, nil, 1)
+}
+
+func (c DatadogStatsClient) BumpSum(key string, val float64) {
+	// Sum can go only up, so I gues Count is best aproximate, I'm not
+	// sure how lossy is float to int conversion here
+	// code grep indicates that method is usually called with value of 1
+	c.client.Count(key, int64(val), nil, 1)
+}
+
+func (c DatadogStatsClient) BumpTime(key string) interface {
+	End()
+} {
+	return timeEnd{c, key, time.Now()}
+}
+
+type timeEnd struct {
+	dataDogStatsClient DatadogStatsClient
+	key                string
+	eventStartTime     time.Time
+}
+
+func (n timeEnd) End() {
+	// Graphite default precision is millisecond I think, should switch later
+	// to millisecond I guess
+	n.dataDogStatsClient.client.Gauge(n.key, float64(time.Since(n.eventStartTime).Nanoseconds()), nil, 1)
+}
